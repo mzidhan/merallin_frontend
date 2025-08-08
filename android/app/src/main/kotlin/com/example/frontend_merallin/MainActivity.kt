a892dfe88bf105be8f00de01ceeb19f3e26134c3
zidhanprasetyo <darkness17032004@gmail.com>
2025-08-08 10:17:19 +0700

    Feat: Implement FVM and migrate to Flutter 3.10.0

--- android/app/src/main/kotlin/com/example/frontend_merallin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.frontend_merallin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/merallin_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.merallin_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
